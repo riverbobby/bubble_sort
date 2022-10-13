9aa9dc123bd92394a7fc27b13965ec5a617944db
Justin Townley <justin.d.townley@gmail.com>
2022-10-13 08:58:12 -0700

    Run rubocop autocorrect

--- bubble_sort.rb
+++ bubble_sort.rb
@@ -1,28 +1,26 @@
 require 'pry-byebug'
 
 def bubble_sort(array)
-	last_index = array.count - 1
-	
-	loop do
-		swapped = false
-		
-		for i in 0..last_index - 1 do
-			if array[i] > array[i + 1]
-				temp = array[i]
-				array[i] = array[i + 1]
-				array[i + 1] = temp
-				swapped = true
-			end
-		end
+  last_index = array.count - 1
 
-		last_index = last_index - 1
+  loop do
+    swapped = false
 
-		if swapped == false || last_index <= 0
-			break			
-		end
-	end
+    for i in 0..last_index - 1 do
+      next unless array[i] > array[i + 1]
 
-	return array
+      temp = array[i]
+      array[i] = array[i + 1]
+      array[i + 1] = temp
+      swapped = true
+    end
+
+    last_index -= 1
+
+    break if swapped == false || last_index <= 0
+  end
+
+  array
 end
 
-p bubble_sort([1,4,3,78,2,10,2])
+p bubble_sort([1, 4, 3, 78, 2, 10, 2])
